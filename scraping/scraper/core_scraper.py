ac8257229e96d00254ac06b97d7a7f7862fcc19c
Sophia <sophialimasantos4@gmail.com>
2025-09-27 17:08:43 -0300

    feat: extração para todas as páginas e informações armazenadas em listas de dicionários

--- scraping/scraper/core_scraper.py
+++ scraping/scraper/core_scraper.py
@@ -8,67 +8,52 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 SELETOR_NOME = "h3.font-bold.text-base.__className_cf1728"
 SELETOR_DESCRICAO = "p.text-justify.line-clamp-4"
 SELETOR_AREA = "div.bg-popover.text-primary:not(.h-6):not(.inline-flex)"
-SELETOR_PROXIMO_BUTTON = ""
 SELETOR_QUANT_PAGS = "p.text-base"
 
-# ---- URL ------
-URL_PAGINA = "https://vitrine.sebraestartups.com.br/?state=BA"
-
 def raspar_dados_empresas(driver):
-    print(f"Acessando a página: {URL_PAGINA}")
-    driver.get(URL_PAGINA)
-    wait = WebDriverWait(driver, 10)
-    dados_empresas_ba = {} # Inicializa o dicionário
+    dados_empresas_ba = [] # Inicializa o dicionário
 
     try:
-        # --- DADOS GERAIS ---
-        # Extrai nomes
-        print("Aguardando os elementos carregarem...")
-        h3_nome_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, SELETOR_NOME)))
-        nome_empresas = [elem.text for elem in h3_nome_elements]
-        print("Nomes da empresas carregadas:")
-        for nome in nome_empresas:
-            print(nome)
-            
-        # Extrai descrições    
-        p_descricao_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, SELETOR_DESCRICAO)))
-        descricao_empresas = [elem.text for elem in p_descricao_elements]
-        print("Descrições carregadas:")
-        for descricao in descricao_empresas:
-            print(descricao)
-        
-        trecho_contem_numPagina_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, SELETOR_QUANT_PAGS)))
-        
         # Extrai a última parte que é o número de páginas
+        '''
+        trecho_contem_numPagina_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, SELETOR_QUANT_PAGS)))
         partes_trecho = trecho_contem_numPagina_element.text.split()
         strPaginas = partes_trecho[-1]
         total_paginas = int(strPaginas)
         print(f"Total de páginas é {total_paginas}")
-
-        # Extrai áreas das empresas
-        div_area_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, SELETOR_AREA)))
-        areas_empresas = [elem.text for elem in div_area_elements if elem.text != "Outros"]
-        print("Áreas das empresas:")
-        for area in areas_empresas:
-            print(area)
-        
-        #for pg in total_paginas:
-        #    url = f'https://vitrine.sebraestartups.com.br/?state=BA&page={pg}'
-
         '''
-        print("\n--- INICIANDO EXTRAÇÃO DE PROJETOS DE PESQUISA ---")
-        projetos_button = wait.until(EC.presence_of_element_located((By.XPATH, XPATH_BOTAO_PROJETOS)))
-        driver.execute_script("arguments[0].scrollIntoView({inline: 'center', block: 'nearest'});", projetos_button)
-        time.sleep(1)
-        driver.execute_script("arguments[0].click();", projetos_button)'''
-        
-        time.sleep(2)
-        
-        # --- ETAPA 5: Montar o resultado final ---
-        dados_empresas_ba = {
-            "nome": nome_empresas,
-            "descricao": descricao_empresas
-        }
+        total_paginas = 29
+        wait = WebDriverWait(driver, 10)
+
+        for pg in range (1, total_paginas + 1):
+            url_pagina = f'https://vitrine.sebraestartups.com.br/?state=BA&page={pg}'
+            print(f"Acessando a página: {url_pagina}")
+            driver.get(url_pagina)
+
+            # Extrai nomes
+            print("Aguardando os elementos carregarem...")
+            h3_nome_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, SELETOR_NOME)))
+            nome_empresas = [elem.text for elem in h3_nome_elements]
+                
+            # Extrai descrições    
+            p_descricao_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, SELETOR_DESCRICAO)))
+            descricao_empresas = [elem.text for elem in p_descricao_elements]
+            
+            # Extrai áreas das empresas
+            div_area_elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, SELETOR_AREA)))
+            areas_empresas = [elem.text for elem in div_area_elements]
+
+            for nome, descricao, area in zip(nome_empresas, descricao_empresas, areas_empresas):
+                empresa = {
+                    'nome': nome,
+                    'descricao': descricao,
+                    'area': area
+                }
+                dados_empresas_ba.append(empresa)
+                print(f"Empresa adicionada: {nome}")
+
+                for chave,valor in empresa.items():
+                    print(f"{chave.capitalize()}:{valor}\n")
         
         return dados_empresas_ba
 
