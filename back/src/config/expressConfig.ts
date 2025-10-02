0ab1f7923a708c92cae87a5ce3e03cdc0229a289
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-10-02 02:55:20 -0300

    feat: move o expressConfig e faz utilizar os routers

--- back/src/config/expressConfig.ts
+++ back/src/config/expressConfig.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors, { CorsOptions } from 'cors';
+import router from '@/routes/index.js';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ const options: CorsOptions = {
 app.use(cors(options));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', router);
 
 export default app;
 
