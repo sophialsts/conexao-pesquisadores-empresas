148e0849133a60b0da119e11182e8f1e5693fa70
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-10-02 02:54:02 -0300

    feat: adiciona services para pesquisadores e empresas

---
+++ back/src/services/companies.services.ts
@@ -0,0 +1,73 @@
+import prisma from "@/config/prismaClient.js"
+import { getEmbedding } from "@/utils/index.js";
+import { Company, RecommendedCompany } from "@/types/companies.types.js";
+import { companyWithIdAndName } from '@/selectors/companies.selectors.js';
+
+const getCompanies = async (limit:number=10): Promise<Company[]> => {
+    return prisma.companies.findMany({
+        take: limit,
+        select: companyWithIdAndName
+    });
+}
+
+const getCompaniesBySimilarity = async (searchText: string, limit:number=10): Promise<Company[]> => {
+    const queryEmbedding = await getEmbedding(searchText);
+
+    const vectorQuery = `[${queryEmbedding.join(',')}]`;
+
+    const results = await prisma.$queryRaw<Company[]>`
+        SELECT
+        id,
+        name
+        FROM
+        companies
+        ORDER BY
+        embedding <=> ${vectorQuery}::vector ASC
+        LIMIT
+        ${limit};
+    `;
+
+  return results;
+}
+
+const getCompanyById = async (id:string): Promise<Company|null> => {
+    return await prisma.companies.findUnique({
+        where: {
+           id: id, 
+        },
+        select: companyWithIdAndName
+    });
+}
+
+async function getRecommendedCompaniesForResearcher(
+  researcherId: string
+): Promise<RecommendedCompany[]> {
+
+  const recommendations = await prisma.company_recommendations_for_researchers.findMany({
+    where: {
+      researcher_id: researcherId,
+    },
+
+    select: {
+      area: true,
+      recommendation_reason: true,
+      companies: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  const recommendedCompanies: RecommendedCompany[] = recommendations.map((rec) => ({
+    id: rec.companies.id,
+    name: rec.companies.name,
+    area: rec.area,
+    recommendationReason: rec.recommendation_reason,
+  }));
+
+  return recommendedCompanies;
+}
+
+export { getCompanies, getCompaniesBySimilarity, getCompanyById, getRecommendedCompaniesForResearcher };
---
+++ back/src/services/researchers.services.ts
@@ -0,0 +1,96 @@
+import prisma from "@/config/prismaClient.js"
+import { getEmbedding } from "@/utils/index.js";
+import { researcherWithIdAndName } from "@/selectors/researchers.selectors.js";
+import { RecommendedResearcher, Researcher, ResearcherSortBy } from "@/types/researchers.types.js";
+import { Prisma } from "@prisma/client";
+
+const getResearchers = async (limit:number = 10): Promise<Researcher[]> => {
+    return prisma.researchers.findMany({
+        take: limit,
+        select: researcherWithIdAndName
+    });
+}
+
+const getResearchersBySimilarity = async (searchText:string, limit:number = 10): Promise<Researcher[]> => {
+    const queryEmbedding = await getEmbedding(searchText);
+
+    const vectorQuery = `[${queryEmbedding.join(',')}]`;
+
+    const results = await prisma.$queryRaw<Researcher[]>`
+        SELECT
+        researcher_id,
+        name
+        FROM
+        researchers
+        ORDER BY
+        embedding <=> ${vectorQuery}::vector ASC
+        LIMIT
+        ${limit};
+    `;
+
+  return results;
+}
+
+const getResearcherById = async (id:string): Promise<Researcher|null> => {
+      return await prisma.researchers.findUnique({
+        where: {
+           researcher_id: id, 
+        },
+        select: researcherWithIdAndName
+    });
+}
+
+async function getRecommendedResearchersForCompany(
+  companyId: string, sortBy: ResearcherSortBy = 'average'
+): Promise<RecommendedResearcher[]> {
+
+ let orderByClause: Prisma.Sql;
+
+  switch (sortBy) {
+    case 'research_alignment':
+      orderByClause = Prisma.sql`ORDER BY MAX(CASE WHEN eval.criterion_name = 'research_alignment' THEN eval.criterion_value END) DESC NULLS LAST`;
+      break;
+    case 'innovation_potential':
+      orderByClause = Prisma.sql`ORDER BY MAX(CASE WHEN eval.criterion_name = 'innovation_potential' THEN eval.criterion_value END) DESC NULLS LAST`;
+      break;
+    case 'cultural_fit':
+      orderByClause = Prisma.sql`ORDER BY MAX(CASE WHEN eval.criterion_name = 'cultural_fit' THEN eval.criterion_value END) DESC NULLS LAST`;
+      break;
+    case 'relevant_experience':
+      orderByClause = Prisma.sql`ORDER BY MAX(CASE WHEN eval.criterion_name = 'relevant_experience' THEN eval.criterion_value END) DESC NULLS LAST`;
+      break;
+    case 'average':
+    default:
+      orderByClause = Prisma.sql`ORDER BY AVG(eval.criterion_value) DESC NULLS LAST`;
+      break;
+  }
+  
+  const baseQuery = Prisma.sql`
+    SELECT
+      eval.researcher_id AS id,
+      r.name AS name,
+      jsonb_agg(
+        jsonb_build_object(
+          'criterionName', eval.criterion_name,
+          'value', eval.criterion_value
+        )
+      ) AS criteria
+    FROM
+      researcher_evaluations_by_company AS eval
+    JOIN
+      researchers AS r ON eval.researcher_id = r.researcher_id
+    WHERE
+      eval.company_id = ${companyId}::uuid
+    GROUP BY
+      eval.researcher_id, r.name
+  `;
+
+  const finalQuery = Prisma.sql`${baseQuery} ${orderByClause}`;
+
+  const results = await prisma.$queryRaw<RecommendedResearcher[]>(finalQuery);
+
+  return results;
+}
+
+
+export { getResearchers, getResearchersBySimilarity, getResearcherById, getRecommendedResearchersForCompany };
