b3811d21751e0a399c6a4214141e238bbf002e84
Victor Romano Arcuri <victor.r.arcuri@gmail.com>
2025-10-01 21:28:56 -0300

    feature: adiciona componente das empresas sugeridas na busca

---
+++ front/src/components/EmpresasSugeridas.tsx
@@ -0,0 +1,29 @@
+const EmpresasSugeridas = () => {
+    
+    return (
+    <div className="flex mt-1 flex-wrap w-full">
+        <div className="w-full p-2 flex gap-2 max-w-5xl flex-wrap font-medium text-sm">
+            <div className="text-white flex bg-eng-primary rounded p-2 justify-center items-center">
+                <p>Google</p>
+            </div>
+            <div className="text-white flex bg-eng-primary rounded p-2 justify-center items-center">
+                <p>Amazon</p>
+            </div>
+            <div className="text-white flex bg-eng-primary rounded p-2 justify-center items-center">
+                <p>Lorem Ipsum Et</p>
+            </div>
+            <div className="text-white flex bg-eng-primary rounded p-2 justify-center items-center">
+                <p>Lorem Ipsum Et</p>
+            </div>
+            <div className="text-white flex bg-eng-primary rounded p-2 justify-center items-center">
+                <p>Lorem Ipsum Et</p>
+            </div>
+            <div className="text-white flex bg-eng-primary rounded p-2 justify-center items-center">
+                <p>Lorem Ipsum Et</p>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export { EmpresasSugeridas }
